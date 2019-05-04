7e4a2abac624c5b201440db9df56fcff05e41f71
Brandon <brandonpeterson2007@gmail.com>
2019-05-04 19:53:50 -0400

    Episode 1
    made: 5/4/2019
    channel: Us And Him

---
+++ Episode 1 - getting the bot setup/bot.js
@@ -0,0 +1,28 @@
+const Botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
+const client = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async => {
+  console.log("i AM ONLINE!!! my name is " + `${bot.user.username}`);
+  bot.user.setActivity(`SUBSCRIBE TO US AND HIM`);
+});
+
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  let prefix = Botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+  if(message.channel.type === "dm") return message.channel.send("Sorry, you can't talk to me in DM's!").then(msg => msg.delete(1500));
+
+if(cmd === `${prefix}test`){
+  return message.reply("Hello, this bot works!");
+}
+
+if(cmd === `Bot-prefix`){
+  return message.channel.send("Hello, my prefix is " + `${prefix}!`)
+}
+});
+
+bot.login(Botconfig.token);
